190fec60ffcbb59676bbc171268a50a99b1e6e89
rwwwx <vlad.matiukhin@gmail.com>
2022-05-03 14:31:22 +0300

    refactored CRUD methods.

--- src/main/java/com/example/annotationtest/service/StudentService.java
+++ src/main/java/com/example/annotationtest/service/StudentService.java
@@ -3,6 +3,7 @@ package com.example.annotationtest.service;
 import com.example.annotationtest.entity.Student;
 import com.example.annotationtest.entity.StudentDTO;
 import com.example.annotationtest.entityRepository.StudentRepo;
+import com.example.annotationtest.entityRepository.UserRepo;
 import com.example.annotationtest.exception.InvalidIdException;
 import com.example.annotationtest.utils.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
@@ -16,38 +17,74 @@ import java.util.List;
 public class StudentService {
 
     private final StudentRepo studentRepo;
+    private final UserRepo userRepo;
 
     @Autowired
-    public StudentService(StudentRepo studentRepo) {
+    public StudentService(StudentRepo studentRepo, UserRepo userRepo) {
         this.studentRepo = studentRepo;
+        this.userRepo = userRepo;
     }
 
+    //TODO refactor
     public Student saveNewStudent(StudentDTO studentDTO) {
-        Student student = UtilityClass.studentDTOToStudent(studentDTO);
+        Student student = UtilityClass.convertToEntity(studentDTO);
         return studentRepo.save(student);
     }
 
     public Student getStudentById(long id) {
-        return studentRepo.getById(id);
+        if (isStudentExists(id)) {
+            var student = studentRepo.getById(id);
+            if (isAdmin())
+                return student;
+            if (isStudentOwnerEqualToCurrentUser(student))
+                return student;
+        }
+        throw new InvalidIdException();
     }
 
     public List<Student> getAllStudents() {
         return studentRepo.findAll();
     }
 
+    //TODO test this method
     @Transactional
-    public Student updateStudent(long id, StudentDTO studentDTO) {
-        return studentRepo.save(new Student
-                (id, UtilityClass.studentDTOToStudent(studentDTO)));
+    public Student updateStudent(long id, StudentDTO studentDTO) throws InvalidIdException {
+        if (isStudentExists(id)) {
+            var studentForUpdate = studentRepo.getById(id);
+            UtilityClass.modifyStudentFieldsFromDTO(studentForUpdate, studentDTO);
+            if (isAdmin())
+                return studentRepo.save(studentForUpdate);
+            if (isStudentOwnerEqualToCurrentUser(studentForUpdate))
+                return studentRepo.save(studentForUpdate);
+        }
+        throw new InvalidIdException();
     }
 
     @Transactional
     public void deleteStudent(long id) throws InvalidIdException {
-        if (studentRepo.existsById(id)) {
-            studentRepo.deleteById(id);
-        } else {
-            throw new InvalidIdException();
+        if (isStudentExists(id)) {
+            if (isAdmin())
+                studentRepo.deleteById(id);
+            if (isStudentOwnerEqualToCurrentUser(studentRepo.getById(id)))
+                studentRepo.deleteById(id);
         }
+        throw new InvalidIdException();
+    }
+
+    private boolean isStudentExists(long id) {
+        return studentRepo.existsById(id);
+    }
+
+    private String getRoleOfCurrentUser() {
+        return userRepo.getByEmail(UtilityClass.getEmailOfCurrentUser()).getRole().name();
+    }
+
+    private boolean isAdmin() {
+        return getRoleOfCurrentUser().equals("ADMIN");
+    }
+
+    private boolean isStudentOwnerEqualToCurrentUser(Student student) {
+        return student.getOwner().getEmail().equals(UtilityClass.getEmailOfCurrentUser());
     }
 
 }
