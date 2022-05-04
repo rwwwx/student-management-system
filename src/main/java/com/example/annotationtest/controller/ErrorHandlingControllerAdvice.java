3b9fd4daa3b86dce88931edb09422fa0b260f529
rwwwx <vlad.matiukhin@gmail.com>
2022-05-04 11:11:13 +0300

    refactored methods and added new handler.

--- src/main/java/com/example/annotationtest/controller/ErrorHandlingControllerAdvice.java
+++ src/main/java/com/example/annotationtest/controller/ErrorHandlingControllerAdvice.java
@@ -1,12 +1,12 @@
 package com.example.annotationtest.controller;
 
+import com.example.annotationtest.exception.InvalidAccessException;
 import com.example.annotationtest.exception.InvalidEmailException;
 import com.example.annotationtest.exception.InvalidIdException;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,38 +18,46 @@ import javax.validation.ConstraintViolationException;
 @ControllerAdvice
 public class ErrorHandlingControllerAdvice {
 
+    private static final String MASSAGE_FOR_LOGGER = "ErrorHandlingControllerAdvice catch an exception ";
+
     public ErrorHandlingControllerAdvice() {
         log.info("ErrorHandlingControllerAdvice init");
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException e) {
-        log.error("ErrorHandlingControllerAdvice catch an exception", e);
+        log.error(MASSAGE_FOR_LOGGER, e);
         return new ResponseEntity<>(e. getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidEmailException.class)
     public ResponseEntity<String> handleInvalidEmailException(InvalidEmailException e) {
-        log.error("ErrorHandlingControllerAdvice catch an exception ", e);
+        log.error(MASSAGE_FOR_LOGGER, e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException e) {
-        log.error("ErrorHandlingControllerAdvice catch an exception ", e);
+        log.error(MASSAGE_FOR_LOGGER, e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidIdException.class)
     public ResponseEntity<String> handleInvalidIdException(InvalidIdException e) {
-        log.error("ErrorHandlingControllerAdvice catch an exception ", e);
+        log.error(MASSAGE_FOR_LOGGER, e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException e) {
-        log.error("ErrorHandlingControllerAdvice catch an exception ", e);
+        log.error(MASSAGE_FOR_LOGGER, e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidAccessException.class)
+    public ResponseEntity<String> handleInvalidAccessException(InvalidAccessException e) {
+        log.error(MASSAGE_FOR_LOGGER, e);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
     }
 
 }
